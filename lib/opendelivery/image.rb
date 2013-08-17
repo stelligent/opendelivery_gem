b8c7cd824b0ca1bdfe58b72a62f245bd970460e6
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-17 18:45:10 -0400

    Refactored the image class

--- lib/opendelivery/image.rb
+++ lib/opendelivery/image.rb
@@ -1,28 +1,43 @@
-class Image
+module OpenDelivery
+  class Image
 
-  def initialize
-    @ec2 = AWS::EC2.new
-    @sdb = AWS::SimpleDB.new
-    @auto_scale = AWS::AutoScaling.new
-  end
+    def initialize
+      @ec2 = AWS::EC2.new
+      @auto_scale = AWS::AutoScaling.new
+      @domain = OpenDelivery::Domain.new
+    end
 
-  def create(as_group_name, image_name, sdb_domain, type, key)
-    domain = Domain.new
-    image = @ec2.images.create(
-      instance_id: @auto_scale.groups[as_group_name].auto_scaling_instances.first.id,
-      name: image_name
-    )
+    def create(domain, stack_name, type, key)
+      prepped_instance = prep_instance(domain stack_name)
+      image = @ec2.images.create(instance_id: prepped_instance, name: stack_name)
+      wait_for_image(image)
+      @domain.set_property(domain, key, type, image.id)
+    end
 
-    sleep 10
+    protected
 
-    while image.state != :available
+    def wait_for_image(image)
       sleep 10
-      case image.state
-      when :failed
-        image.delete
-        raise RuntimeError, 'Image Creation Failed'
+
+      while image.state != :available
+        sleep 10
+        case image.state
+        when :failed
+          image.delete
+          raise RuntimeError, 'Image Creation Failed'
+        end
       end
     end
-    domain.set_property(sdb_domain, key, type, image.id)
+
+    def prep_instance(domain stack_name)
+      group_name = @domain.get_property(domain, stack_name, "AWS::AutoScaling::AutoScalingGroup")
+      instance = @auto_scale.groups[group_name].auto_scaling_instances.first.id
+      @ec2.instances[instance].stop
+
+      while @ec2.instances[instance].status != :stopped
+        sleep 10
+      end
+      return instance
+    end
   end
 end
