aad35c51d0b3b81def1c558ce467b817c5a1cc99
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-17 18:44:40 -0400

    Moved domain loading into the stack creation job since so tightly coupled

--- lib/opendelivery/stack.rb
+++ lib/opendelivery/stack.rb
@@ -1,45 +1,52 @@
-class Stack
+module OpenDelivery
+  class Stack
 
-  def initialize
-    @cfn = AWS::CloudFormation.new
-  end
+    def initialize
+      @cfn = AWS::CloudFormation.new
+      @domain = OpenDelivery::Domain.new
+    end
 
-  def create(template, stack_name, parameters = {}, wait=false)
-    stack = @cfn.stacks.create(stack_name,
-                              File.open(template, "r").read,
-                              :parameters => parameters,
-                              :capabilities => ["CAPABILITY_IAM"],
-                              :disable_rollback => true)
+    def create(template, domain, name, parameters = {}, wait=false)
+      stack = @cfn.stacks.create(name,
+        File.open(template, "r").read,
+        :parameters => parameters,
+        :capabilities => ["CAPABILITY_IAM"],
+        :disable_rollback => true)
 
-    if wait
-      while stack.status != "CREATE_COMPLETE"
-        sleep 30
+      if wait
+        wait_for_stack(stack)
+      end
 
-        case stack.status
-        when "ROLLBACK_IN_PROGESS"
-          stack.delete
-        when "ROLLBACK_COMPLETE"
-          stack.delete
-        end
+      stack.resources.each do |resource|
+        @domain.set_property(domain, name, resource.resource_type, resource.physical_resource_id)
       end
     end
-  end
 
-  def destroy(stack_name)
-    stack = @cfn.stacks[stack_name]
-    stack.delete
-    while stack.exists?
-      sleep 30
+    def destroy(domain, name)
+      stack = @cfn.stacks[name]
+      stack.delete
+      while stack.exists?
+        sleep 20
+      end
+      @domain.destroy_item(domain, name)
     end
-  end
 
-  def list
-    @cfn.stacks.each do |stack|
-      case stack.status
-      when "CREATE_COMPLETE"
-        puts "Stack Name: #{stack.name} | Status: Available"
-      when "CREATE_IN_PROGRESS"
-        puts "Stack Name: #{stack.name} | Status: In Progress"
+    def list
+      @cfn.stacks.each do |stack|
+        puts "Stack Name: #{stack.name} | Status: #{stack.status}"
+      end
+    end
+
+    protected
+
+    def wait_for_stack(stack)
+      while stack.status != "CREATE_COMPLETE"
+        sleep 20
+
+        case stack.status
+        when "ROLLBACK_IN_PROGESS" || "ROLLBACK_COMPLETE"
+          stack.delete
+        end
       end
     end
   end
