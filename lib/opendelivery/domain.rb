2be05769be57d60fd18c13fdf8614b394c07077f
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-17 18:45:45 -0400

    Added open delivery module

--- lib/opendelivery/domain.rb
+++ lib/opendelivery/domain.rb
@@ -1,44 +1,44 @@
-require 'aws-sdk'
+module OpenDelivery
+  class Domain
 
-class Domain
-
-  def initialize
-    @sdb = AWS::SimpleDB.new
-  end
-
-  def create(domain_name)
-    AWS::SimpleDB.consistent_reads do
-      @sdb.domains.create(domain_name)
+    def initialize
+      @sdb = AWS::SimpleDB.new
     end
-  end
 
-  def destroy(domain_name)
-    @sdb.domains[domain_name].delete
-  end
-
-  def destroy_item(domain_name, item_name)
-    @sdb.domains[domain_name].items[item_name].delete
-  end
-
-  def get_property(sdb_domain, item_name, key)
-    AWS::SimpleDB.consistent_reads do
-      item = @sdb.domains[sdb_domain].items[item_name]
-
-      item.attributes.each_value do |name, value|
-        if name == key
-          @property_value = value.chomp
-        end
+    def create(domain_name)
+      AWS::SimpleDB.consistent_reads do
+        @sdb.domains.create(domain_name)
       end
     end
 
-    return @property_value
-  end
+    def destroy(domain_name)
+      @sdb.domains[domain_name].delete
+    end
 
-  def set_property(sdb_domain, item_name, property, value)
-    AWS::SimpleDB.consistent_reads do
-      item = @sdb.domains[sdb_domain].items[item_name]
+    def destroy_item(domain_name, item_name)
+      @sdb.domains[domain_name].items[item_name].delete
+    end
 
-      item.attributes.set(property => [value])
+    def get_property(sdb_domain, item_name, key)
+      AWS::SimpleDB.consistent_reads do
+        item = @sdb.domains[sdb_domain].items[item_name]
+
+        item.attributes.each_value do |name, value|
+          if name == key
+            @property_value = value.chomp
+          end
+        end
+      end
+
+      return @property_value
+    end
+
+    def set_property(sdb_domain, item_name, property, value)
+      AWS::SimpleDB.consistent_reads do
+        item = @sdb.domains[sdb_domain].items[item_name]
+
+        item.attributes.set(property => [value])
+      end
     end
   end
 end
