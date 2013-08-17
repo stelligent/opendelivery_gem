9fb0014c6483a496c5141f43e0c60a38428f5e47
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-16 21:22:21 -0400

    Removed app path loading. This is no longer needed in the gem as the lib/ directory figures it out

--- config/initialize.rb
+++ config/initialize.rb
@@ -1,15 +1,2 @@
-require 'rubygems'
-require 'aws-sdk'
-require 'trollop'
-
 load File.expand_path('c:\aws\aws.config')
 APP_PATH = File.expand_path('../../', __FILE__) + '/'
-
-require APP_PATH + 'lib/database.rb'
-require APP_PATH + 'lib/domain.rb'
-require APP_PATH + 'lib/image.rb'
-require APP_PATH + 'lib/instance.rb'
-require APP_PATH + 'lib/key_pair.rb'
-require APP_PATH + 'lib/stack.rb'
-require APP_PATH + 'lib/storage.rb'
-require APP_PATH + 'lib/artifact.rb'
