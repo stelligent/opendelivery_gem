467593e25235a4cb9679493df32c507d74012627
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-17 18:45:53 -0400

    Added open delivery module

--- lib/opendelivery/artifact.rb
+++ lib/opendelivery/artifact.rb
@@ -1,10 +1,12 @@
-class Artifact
-  def initialize
-  end
+module OpenDelivery
+  class Artifact
+    def initialize
+    end
 
-  def add_timestamp(build_identifier, artifact)
-    timestamp = Time.now.strftime("%Y.%m.%d.%H.%M.%S.%L")
-    stamped_artifact = "#{artifact}-#{build_identifier}-#{timestamp}"
-    return stamped_artifact
+    def add_timestamp(artifact, build_identifier)
+      timestamp = Time.now.strftime("%Y.%m.%d.%H.%M.%S.%L")
+      stamped_artifact = "#{artifact}-#{build_identifier}-#{timestamp}"
+      return stamped_artifact
+    end
   end
 end
