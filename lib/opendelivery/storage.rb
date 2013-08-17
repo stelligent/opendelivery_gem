cce1d6be4d7c5fb0fabfcc4f94a4138578b4910c
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-17 18:41:46 -0400

    Removed copy method

--- lib/opendelivery/storage.rb
+++ lib/opendelivery/storage.rb
@@ -1,26 +1,24 @@
-class Storage
+module OpenDelivery
+  class Storage
 
-  def initialize
-    @s3 = AWS::S3.new
-  end
+    def initialize
+      @s3 = AWS::S3.new
+    end
 
-  def copy(bucket, key, desired_key)
-    @s3.buckets[bucket].objects[key].copy_to(desired_key)
-  end
+    def upload(file, bucket, key)
+      @s3.buckets[bucket].objects[key].write(:file => file)
+    end
 
-  def upload(file, bucket, key)
-    @s3.buckets[bucket].objects[key].write(:file => file)
-  end
+    def download(bucket, key, output_directory)
+      obj = @s3.buckets[bucket].objects[key]
 
-  def download(bucket, key, output_directory)
-    obj = @s3.buckets[bucket].objects[key]
+      base = Pathname.new("#{obj.key}").basename
 
-    base = Pathname.new("#{obj.key}").basename
+      Dir.mkdir(output_directory) unless File.exists?(output_directory)
 
-    Dir.mkdir(output_directory) unless File.exists?(output_directory)
-
-    File.open("#{output_directory}/#{base}", 'w') do |file|
-      file.write(obj.read)
+      File.open("#{output_directory}/#{base}", 'w') do |file|
+        file.write(obj.read)
+      end
     end
   end
 end
