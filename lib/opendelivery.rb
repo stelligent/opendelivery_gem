4d7506fa178b401662617d17a9a4c917976ca849
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-17 18:41:28 -0400

    Removed unused classes

--- lib/opendelivery.rb
+++ lib/opendelivery.rb
@@ -1,7 +1,5 @@
 require 'opendelivery/artifact.rb'
 require 'opendelivery/domain.rb'
 require 'opendelivery/image.rb'
-require 'opendelivery/instance.rb'
-require 'opendelivery/key_pair.rb'
 require 'opendelivery/stack.rb'
 require 'opendelivery/storage.rb'
