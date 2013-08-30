14a8a45240e5ce40985c8f552eba3ccc333f138d
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-29 22:29:18 -0400

    changed from build identifier to identifier

--- lib/opendelivery/storage.rb
+++ lib/opendelivery/storage.rb
@@ -43,9 +43,9 @@ module OpenDelivery
       end
     end
 
-    def add_timestamp(build_identifier, artifact)
+    def add_timestamp(identifier, artifact)
       timestamp = Time.now.strftime("%Y.%m.%d.%H.%M.%S.%L")
-      stamped_artifact = "#{artifact}-#{build_identifier}-#{timestamp}"
+      stamped_artifact = "#{artifact}-#{identifier}-#{timestamp}"
       return stamped_artifact
     end
   end
