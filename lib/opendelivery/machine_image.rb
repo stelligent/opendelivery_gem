2c176eecccea3f2d891c3f0c08a9c18f7854291c
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-30 17:54:20 -0400

    Added machine image class and removed image

--- lib/opendelivery/machine_image.rb
+++ lib/opendelivery/machine_image.rb
@@ -1,23 +1,37 @@
 require 'aws-sdk'
 
 module OpenDelivery
-  class Image
+  class MachineImage
 
     def initialize(region=nil)
       if region.nil?
         @ec2 = AWS::EC2.new
         @sdb = AWS::SimpleDB.new
         @auto_scale = AWS::AutoScaling.new
+        @domain = OpenDelivery::Domain.new
       else
         @ec2 = AWS::EC2.new(:region => region)
         @sdb = AWS::SimpleDB.new(:region => region)
         @auto_scale = AWS::AutoScaling.new(:region => region)
+        @domain = OpenDelivery::Domain.new(:region => region)
       end
     end
 
-    protected
+    def create(domain, stack_name, type, key)
+      instance_id = prep_instance(domain, stack_name)
+      image = @ec2.images.create(
+        instance_id: instance_id,
+        name: image_name)
+
+      wait_for_image(image)
+
+      @domain.set_property(domain, key, type, image.id)
+    end
+
+    private
 
     def wait_for_image(image)
+      # Waiting for AWS to realize the image is ready to start
       sleep 10
 
       while image.state != :available
