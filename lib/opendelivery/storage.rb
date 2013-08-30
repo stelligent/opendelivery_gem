4e275a278962721d8b9f343f2cc3590002036a1c
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-29 21:41:10 -0400

    Merging changes into open delivery to rescue on key or bucket not existing

--- lib/opendelivery/storage.rb
+++ lib/opendelivery/storage.rb
@@ -1,5 +1,3 @@
-require 'aws-sdk'
-
 module OpenDelivery
   class Storage
 
@@ -15,19 +13,31 @@ module OpenDelivery
       @s3.buckets[bucket].objects[key].copy_to(desired_key)
     end
 
-    def upload(file, bucket, key)
+    def upload(bucket, file, key)
       @s3.buckets[bucket].objects[key].write(:file => file)
     end
 
     def download(bucket, key, output_directory)
+      # Puke if the bucket doesn't exist
+      raise "Bucket #{bucket} doesn't exist" unless @s3.buckets[bucket].exists?
+
+      # Puke if the key doesn't exist. This can cause problems with some bucket policies, so we catch and re-throw
+      begin
+        raise "File with key #{key} doesn't exist in bucket #{bucket}" unless @s3.buckets[bucket].objects[key].exists?
+      rescue Exception => e
+        raise "Exception [#{e.message}] occurred downloading key #{key} from bucket #{bucket}"
+      end
+
       obj = @s3.buckets[bucket].objects[key]
 
       base = Pathname.new("#{obj.key}").basename
 
       Dir.mkdir(output_directory) unless File.exists?(output_directory)
 
-      File.open("#{output_directory}/#{base}", 'w') do |file|
-        file.write(obj.read)
+      File.open("#{output_directory}/#{base}", 'wb') do |file|
+        obj.read do |chunk|
+          file.write(chunk)
+        end
       end
     end
   end
