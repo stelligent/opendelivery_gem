f2080b06f8853a613cbc7523d022e3db67157c8d
Brian Jakovich <brian.jakovich@stelligent.com>
2013-08-30 17:53:33 -0400

    Updated image class to machineimage

--- lib/opendelivery.rb
+++ lib/opendelivery.rb
@@ -1,6 +1,6 @@
 require 'opendelivery/source_control.rb'
 require 'opendelivery/domain.rb'
-require 'opendelivery/image.rb'
+require 'opendelivery/machine_image.rb'
 require 'opendelivery/stack.rb'
 require 'opendelivery/storage.rb'
 
