70e234d186dbb9acd13faf74f4e0646a15c797cf
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-10-23 20:19:02 -1000

    gonna add a job to run these tests headlessly, so I'm making it look for the windows version of the aws config file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,8 @@ end
 require 'aws-sdk'
 require File.expand_path('../../lib/opendelivery/domain.rb', __FILE__)
 require File.expand_path('../../lib/opendelivery/stack.rb', __FILE__)
-load File.expand_path("/opt/aws/aws.config")
+
+# choose your own adventure!
+# load File.expand_path("~/.aws/aws.config")
+# load File.expand_path("/opt/aws/aws.config")
+load File.expand_path("c:\\aws\\aws.config")
