bf2e5337ab615550510b28828724003f4557ef3a
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-02-13 17:48:48 -0600

    don't fail if you can't find creds file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,11 @@ require 'aws-sdk'
 require File.expand_path('../../lib/opendelivery/domain.rb', __FILE__)
 require File.expand_path('../../lib/opendelivery/stack.rb', __FILE__)
 
+
 # choose your own adventure!
-# load File.expand_path("~/.aws/aws.config")
- load File.expand_path("/opt/aws/aws.config")
-#load File.expand_path("c:\\aws\\aws.config")
+#config_file = "~/.aws/aws.config"
+#config_file = "c:\\aws\\aws.config"
+config_file = "/opt/aws/aws.config"
+if File.exist?(config_file)
+  load File.expand_path(config_file)
+end
