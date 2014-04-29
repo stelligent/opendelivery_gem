577b9ea05e96b83b321f1b784058c347b276e225
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-28 20:13:24 -0500

    copying build.sh to commit.sh

---
+++ pipeline/commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+echo checking out revision $SHA
+git checkout $SHA
+
+# For some reason, the Jenkins user doesn't have the path configured correctly...
+PATH=$PATH:/usr/local/bin/
+echo path: $PATH
+
+# check the syntax of each ruby file
+echo Syntax check...
+find . -name *.rb | xargs -n1 ruby -c > /dev/null
+
+# Prep and run unit tests
+gem install bundler
+bundle install
+bundle exec rspec spec/ -f d --tag ~slow
