b184c19cefdc2b54d4546696966f91a7cb1ffac5
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-04-28 19:59:15 -0500

    adding job dsl. this should make all the jobs we need.

---
+++ pipeline/jobs/jobdsl.groovy
@@ -0,0 +1,42 @@
+pipelines = []
+
+pipelines.add(["acceptance", "commit", "trigger", "clean-gemset", "deploy-to-ruby-gems"])
+pipelines.each { jobs ->
+for (i = 0; i < jobs.size; ++ i) {
+    job {
+        name "${jobs[i]}-dsl"
+        scm {
+            git("https://github.com/stelligent/honolulu_answers.git", "master") { node ->
+                node / skipTag << "true"
+            }
+        }
+      if (jobs[i].equals("trigger-stage")) {
+          triggers {
+            scm("* * * * *")
+          }
+      }
+      steps {
+        shell("pipeline/${jobs[i]}.sh")
+        if (i + 1 < jobs.size) {
+          downstreamParameterized {
+            trigger ("${jobs[i+1]}-dsl", "ALWAYS"){
+              currentBuild()
+              propertiesFile("environment.txt")
+            }
+          }
+        }
+      }
+      wrappers {
+          rvm("2.0.0")
+      }
+      publishers {
+        extendedEmail("jonny@stelligent.com", "\$PROJECT_NAME - Build # \$BUILD_NUMBER - \$BUILD_STATUS!", """\$PROJECT_NAME - Build # \$BUILD_NUMBER - \$BUILD_STATUS:
+
+  Check console output at \$BUILD_URL to view the results.""") {
+            trigger("Failure")
+            trigger("Fixed")
+        }
+      }
+    }
+  }
+}
