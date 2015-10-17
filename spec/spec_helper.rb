83f5fda93caa465a227c9a20829cf3969c35ad44
Eric Kascic <eric.kascic@stelligent.com>
2015-10-18 00:07:27 +0100

    #3 first go at trying to put a release process in a custom circle-ci yams file
    kill some cruft in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,3 @@ require 'simplecov'
 SimpleCov.start do
   add_filter 'vendor'
 end
-
-require 'aws-sdk'
-require File.expand_path('../../lib/opendelivery/domain.rb', __FILE__)
-require File.expand_path('../../lib/opendelivery/stack.rb', __FILE__)
-
-
-# choose your own adventure!
-#config_file = "~/.aws/aws.config"
-#config_file = "c:\\aws\\aws.config"
-config_file = "/opt/aws/aws.config"
-if File.exist?(config_file)
-  load File.expand_path(config_file)
-end
