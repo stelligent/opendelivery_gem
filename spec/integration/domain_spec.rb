4279a9e5f78b2c3ccd83acf86b402032df6cde09
Eric Kascic <eric.kascic@stelligent.com>
2015-10-18 16:37:32 +0100

    #4 try paying attention for once

--- spec/integration/domain_spec.rb
+++ spec/integration/domain_spec.rb
@@ -65,7 +65,7 @@ describe OpenDelivery::Domain do
         actual_value = @domain_under_test.get_property(@domain_name, 'test', 'testFieldOne')
         actual_value.should eql 'testValueOne'
 
-        actual_value = @domain_under_test.get_property(@domain_name, 'test', 'testValueTwoA')
+        actual_value = @domain_under_test.get_property(@domain_name, 'test', 'testFieldTwo')
         actual_value.should eql 'testValueTwoA'
       end
     end
