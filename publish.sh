703d853853db1b2e39b7ea1257bd8d1ea72ea818
Eric Kascic <eric.kascic@stelligent.com>
2015-10-18 16:56:10 +0100

    #4 debugging issue computation.  something funny because "head" is ambiguous

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+set -o pipefail
 
 if [[ -z ${rubygems_api_key} ]];
 then
@@ -9,11 +10,14 @@ fi
 git config --global user.email "build@build.com"
 git config --global user.name "build"
 
+set +e
 echo :rubygems_api_key: ${rubygems_api_key} > ~/.gem/credentials
+set -e
 chmod 0600 ~/.gem/credentials
 
 #gem used manual versioning up through 0.4.3, so start off
 #at 0.4.4 and just keep "patching" through 0.4.N
+#might have been less surprising to restart at 0.5.x given we are nuking so much
 current_version=$(ruby -e 'tags=`git tag -l v0\.4\.*`' \
                        -e 'p tags.lines.map { |tag| tag.sub(/v0.4./, "").chomp.to_i }.max')
 
